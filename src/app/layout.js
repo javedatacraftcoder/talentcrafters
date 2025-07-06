074cd28cc8bc26397577fb638df58f33541a36d0
javedatacraftcoder <javier.velazquez@datacraftcoders.com>
2025-07-06 07:54:19 -0600

    V1.8-UnderConstruction

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,23 +22,20 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Bootstrap 5.0.2 CSS CDN */}
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossOrigin="anonymous"
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
 
         {/* Bootstrap 5.0.2 JS CDN (via next/script, after page loads) */}
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        <script 
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
           crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+          defer
+        ></script>
       </body>
     </html>
   );
