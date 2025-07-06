53a904d0cad39c9b29f2ec75065e47f0e2ec5c3e
javedatacraftcoder <javier.velazquez@datacraftcoders.com>
2025-07-06 10:08:08 -0600

    V2.1-1stDeployment-nextouthsecret

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
 const handler = NextAuth({
   providers: [
     GoogleProvider({
