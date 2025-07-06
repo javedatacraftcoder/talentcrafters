49310675c96b287f8f901b3422a4b7038ac8e7b3
javedatacraftcoder <javier.velazquez@datacraftcoders.com>
2025-07-05 23:12:58 -0600

    V1-underconstruction

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-Li9E6k5jle5M+BLwZ19eMyZJ28H+fNdrFf0I8kfuKT8mC5nHj4Wb3n5yAq1AK5VS"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ENjdO4Dr2bkBIFxQpeo3lWzp6V96Dk+W8s1ZrDNpTcZplENYx04ecVIK60Qv2Q8="
+          crossOrigin="anonymous"
+        ></script>
       </body>
     </html>
   );
