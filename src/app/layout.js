aca9484a26688b915ff471e75269155784aa5e7c
javedatacraftcoder <javier.velazquez@datacraftcoders.com>
2025-07-06 08:19:45 -0600

    V1.10-UnderConstruction

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // ✅ Bootstrap sí funciona así
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css"; // Bootstrap se carga aquí
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
