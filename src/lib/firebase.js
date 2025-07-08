df240778f82d91f5d2d2bb185fa6b71d11405608
javedatacraftcoder <javier.velazquez@datacraftcoders.com>
2025-07-07 21:48:26 -0600

    V3.3-Firebase-enviroment

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCgX2a_zf8sweWMGLZa-chIdAKTSC5kudI",
-  authDomain: "talentcrafters-19f35.firebaseapp.com",
-  projectId: "talentcrafters-19f35",
-  storageBucket: "talentcrafters-19f35.firebasestorage.app",
-  messagingSenderId: "764716169339",
-  appId: "1:764716169339:web:c6e2c1c98a3912d9d90ade",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Evita reinicializar si ya está inicializado
