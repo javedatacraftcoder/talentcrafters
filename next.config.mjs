5986b56c54c15f306384e1ff794d5100b35a0cb1
javedatacraftcoder <javier.velazquez@datacraftcoders.com>
2025-08-16 20:56:33 -0600

    V10.341-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+    // o, con más control:
+    // remotePatterns: [{ protocol: 'https', hostname: 'lh3.googleusercontent.com' }],
+  },
+};
 
 export default nextConfig;
